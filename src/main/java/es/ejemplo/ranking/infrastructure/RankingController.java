45d11f6c15c9c1375b912724c09f36f644b06756
Fernando Cano <fccano@gmail.com>
2022-01-09 14:03:55 +0100

    Basic json response controller added

--- src/main/java/es/ejemplo/ranking/infrastructure/RankingController.java
+++ src/main/java/es/ejemplo/ranking/infrastructure/RankingController.java
@@ -2,9 +2,13 @@ package es.ejemplo.ranking.infrastructure;
 
 import es.ejemplo.ranking.application.TopRankingCalculatorService;
 import es.ejemplo.ranking.domain.ItemInRank;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/ranking")
@@ -21,6 +25,18 @@ public final class RankingController {
     @ResponseBody
     public List<String> getTopPopularRepos(@RequestParam String language, @RequestParam int maxInRank) {
         return topRankingCalculatorService.calculate(language,maxInRank);
+    }
+
+    //Se puede invocar así: http://localhost:8080/ranking/jsonTop?language=JavaScript&maxInRank=2
+    @GetMapping("/jsonTop")
+    @ResponseBody
+    public List<HashMap<String, String>>  getJsonTopPopularRepos(@RequestParam String language, @RequestParam int maxInRank) {
+        List<String> topRespuesta = topRankingCalculatorService.calculate(language,maxInRank);
+
+        return topRespuesta.stream().map(response -> new HashMap<String, String>() {{
+            put("repo", response);
+
+        }}).collect(Collectors.toList());
     }
 
 
--- src/test/java/es/ejemplo/ranking/infrastructure/RankingControlerTest.java
+++
@@ -1,8 +0,0 @@
-package es.ejemplo.ranking.infrastructure;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class RankingControlerTest {
-
-
-}
